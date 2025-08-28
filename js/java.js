12a8d84c791560d00980f13d6c7a5e3ff6a6c796
sohelma <sohelma.us@gmail.com>
2025-08-29 00:53:20 +0600

    added

--- js/java.js
+++ js/java.js
@@ -1,3 +1,4 @@
+//Love icon count ---------
 const loveCount = document.getElementById("love-count");
 let count = 0;
 
@@ -6,5 +7,37 @@ for (let i = 0; i < loveButtons.length; i++) {
     loveButtons[i].addEventListener("click", function() {
         count++;
         loveCount.innerText = count;
+    });
+}
+
+
+//call btn and history ---------
+const coinCountEl = document.getElementById("coin-count");
+const historyList = document.getElementById("history-list");
+
+const callButtons = document.getElementsByClassName("call-btn"); 
+
+for (let i = 0; i < callButtons.length; i++) {
+    callButtons[i].addEventListener("click", function() {
+        let coin = parseInt(coinCountEl.textContent);
+
+        if (coin < 20) {
+            alert("You have not enough coin,needed atleast 20 coin ");
+            return;
+        }
+        const name = this.getAttribute("data-name");
+        const number = this.getAttribute("data-number");
+
+        alert(`${name}, ${number}`);
+
+        coin -= 20;
+        coinCountEl.textContent = coin;
+
+        const now = new Date();
+        const time = now.toLocaleTimeString();
+
+        const li = document.createElement("li");
+        li.textContent = `${name}, ${number} at ${time}`;
+        historyList.appendChild(li);
     });
 }
