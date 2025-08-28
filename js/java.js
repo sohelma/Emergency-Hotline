8156594a78fdef14cfc3fa367ec1a3cbe56b0c2f
sohelma <sohelma.us@gmail.com>
2025-08-28 21:11:08 +0600

    addedmore

--- js/java.js
+++ js/java.js
@@ -1,4 +1,10 @@
-document.getElementById('love')
-.addEventListener('click',function(){
-   console.log('click on love') ;
-})
+const loveCount = document.getElementById("love-count");
+let count = 0;
+
+const loveButtons = document.getElementsByClassName("love"); 
+for (let i = 0; i < loveButtons.length; i++) {
+    loveButtons[i].addEventListener("click", function() {
+        count++;
+        loveCount.innerText = count;
+    });
+}
